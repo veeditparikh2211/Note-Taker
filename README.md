aa41ba7cb60bbd1f84604aee10f0aa5ffd7fc050
veeditparikh2211 <veeditparikh2211@gmail.com>
2022-02-11 18:59:10 -0500

    all routes working

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Note-Taker
+
+Note taker is an app to add, post, delete notes being created.
+## Purpose
+
+Note Taker is mainly used to add new note by using the get route, post notes by the use of post route and delete the notes created by using the delete route.
+## Built With
+
+Node
+Express js
+## Website
+
+
+
+## Application
+
+![](images/Screenshot_1.PNG)
+![](images/Deletefunctionality.PNG)
+
+
+## Contribution
+
+Made by Veedit Parikh
--- server.js
+++ server.js
@@ -7,80 +7,85 @@ const app = express();
 const fs = require("fs");
 const path = require("path");
 
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
 app.get("/", function(req, res) {
-    console.log("heereeeeeee");
+
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 app.get("/notes", function(req, res) {
-    console.log("hiiiiiiiiiiiiiiii");
+
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-
+// get route
 
 app.get("/api/notes", (req, res) => {
 
     //console.log(res.json(database));
-    return res.json(database.notes);
+    return res.json(database);
 });
 
-// app.post("/api/notes", (req, res) => {
-//     // req.body is where our incoming content will be
-//     console.log(req.body);
 
-//     req.body.id = database.length.toString();
+// post route
 
-//     const newnote = createNewdata(req.body, database);
+app.post("/api/notes", (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
 
-//     res.json(newnote);
-// });
+    req.body.id = JSON.stringify(database.length + 1);
 
+    const newnote = createNewdata(req.body, database);
 
-// app.delete("/api/notes/:id", (req, res) => {
+    res.json(newnote);
+});
 
-//     const id = req.params.id;
+// delete route
 
-//     deleteNote(id, database);
+app.delete("/api/notes/:id", (req, res) => {
 
-//     return res.json(true);
+    const id = req.params.id;
 
+    deleteNote(id, database);
 
-// });
+    return res.json(true);
+
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
 
-// function createNewdata(body, notesArray) {
-//     const newnote = body;
-//     notesArray.push(newnote);
-//     fs.writeFileSync(
-//         path.join(__dirname, "db/db.json"),
-//         JSON.stringify({ notes: notesArray }, null, 2)
-//     );
-//     console.log(newnote);
-//     return newnote;
-// }
+// POST function
 
-// function deleteNote(id, notesArray) {
-//     for (var i = 0; i < database.length; i++) {
+function createNewdata(body, notesArray) {
 
-//         var note = notesArray[i];
-//         if (note.id == id) {
-//             notesArray.splice(i, 1);
-//             fs.writeFileSync(
-//                 path.join(__dirname, "db/db.json"),
-//                 JSON.stringify({ notes: notesArray }, null, 2)
-//             );
+    console.log("body : ", body);
+    console.log("notesArray : ", notesArray);
+    const newnote = body;
+    notesArray.push(body);
+    fs.writeFileSync(
+        path.join(__dirname, "db/db.json"),
+        JSON.stringify(notesArray));
+    console.log(newnote);
+    return newnote;
+}
 
-//             break;
-//         }
-//     }
-// }
+// DELETE function
+
+function deleteNote(id, notesArray) {
+    for (var i = 0; i < database.length; i++) {
+
+        var note = notesArray[i];
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, "db/db.json"),
+                JSON.stringify(notesArray));
+
+            break;
+        }
+    }
+}
